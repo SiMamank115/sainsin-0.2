76d8437dbe8637465802a0bbb49af6dfb895bad7
Faeest <faeest.115@gmail.com>
2024-01-13 17:08:43 +0700

    load TO

--- script.js
+++ script.js
@@ -67,6 +67,43 @@ function decodeSubtesType(type) {
 	return res;
 }
 let fetchedTryouts;
+async function loadTO(str) {
+	let module = str.split(",");
+	let questions = await getQuestions(module).then((e) => e.json());
+	let menus = ``;
+	questions.forEach((e, idx) => {
+		menus += `<button class='btn bg-base-200 shadow-base-content/20 shadow grow md:basis-40 basis-20'>${idx}</button>`;
+	});
+	console.log(questions);
+	await sleep(100);
+	hero.innerHTML += createAccordion([
+		[menus, "Menu"],
+		["LMAO2", "Question"],
+	]);
+}
+function createAccordion(
+	content = [
+		["", ""],
+		["", ""],
+	]
+) {
+	let opening = '<div class="join join-vertical w-full">';
+	let body = "";
+	let ending = "</div>";
+	content.forEach((e) => {
+		body += `
+		<div class="collapse collapse-arrow join-item border-2 border-base-content">
+			<input type="radio" name="my-accordion-4" checked="checked" /> 
+			<div class="collapse-title text-xl font-medium">
+				${e?.dataset?.title ?? e?.[1] ?? "Click"}
+			</div>
+			<div class="collapse-content flex flex-wrap gap-3"> 
+			${e?.outerHTML ?? e?.[0] ?? "<p>sad</p>"}
+			</div>
+		</div>`;
+	});
+	return opening + body + ending;
+}
 async function crack() {
 	cracker.setAttribute("disabled", true);
 	if (await checkApiKey()) {
@@ -95,7 +132,7 @@ async function crack() {
 				for (let i = 0; i < partition[r].length; i++) {
 					let e = partition[r][i];
 					modaltryoutsbody.appendChild(createToCard(e, hs));
-					await sleep(50);
+					await sleep(_.random(0, 25));
 				}
 			}
 		}
@@ -116,21 +153,26 @@ function createToCard(to, hs) {
 		setToHistory(e.id);
 		let modules = e.modules;
 		if (modules) {
+			modalmodulesbody.innerHTML = "";
 			await modules.forEach(async (t, idx) => {
 				if (t) {
-					console.log(t);
 					let newCard2 = document.createElement("div");
+					let newButton = document.createElement("button");
 					let info = await getModulesInfo(t);
 					if (info) {
 						info[1] = info[1].map((ee) => ee.id);
 						newCard2.classList.add(..."indicator grow flex sm:tooltip".split(" "));
 						newCard2.id = `subtesbutton${e.id}`;
-						newCard2.innerHTML = `<button class="btn bg-base-200 grow w-full">${decodeSubtesType(info[0])} (${t})</button>`;
+						newButton.dataset.module = t;
+						newButton.dataset.question = info[1];
+						newButton.classList.add(..."btn bg-base-200 grow w-full".split(" "));
+						newButton.textContent = `${decodeSubtesType(info[0])} (${t})`;
+						newCard2.appendChild(newButton);
+						newButton.onclick = (e) => loadTO(e.target.dataset.question);
 						modalmodulesbody.appendChild(newCard2);
 					}
 				}
 			});
-			doneCracking();
 			modalmodules.showModal();
 		} else {
 			doneCracking();
@@ -154,25 +196,32 @@ function createToCard(to, hs) {
 	return newCard;
 }
 async function getModulesInfo(id) {
-	let modules = await getModules(id).then((e) => e.json());
+	let modules = await getModules(id, "&select=id,question_id,module_id").then((e) => e.json());
 	if (modules.length > 0) {
-		let type = await getQuestion(modules[0].question_id).then((e) => e.json());
+		let type = await getQuestion(modules[0].question_id, "&select=type").then((e) => e.json());
 		return [type[0].type, modules];
 	} else {
 		return;
 	}
 }
-async function getQuestion(id) {
+async function getQuestion(id, option = "") {
 	var head = new Headers();
 	head.append("apikey", apikey.value);
-	return await fetch(issuer + "questions?id=eq." + (id ?? 0), {
+	return await fetch(issuer + "questions?id=eq." + (id ?? 0) + option, {
 		headers: head,
 	});
 }
-async function getModules(id) {
+async function getQuestions(id, option = "") {
 	var head = new Headers();
 	head.append("apikey", apikey.value);
-	return await fetch(issuer + "module_question?module_id=eq." + (id ?? 0), {
+	return await fetch(issuer + "questions?id=in.(" + (id?.join(",") ?? 0) + ")" + option, {
+		headers: head,
+	});
+}
+async function getModules(id, opstion) {
+	var head = new Headers();
+	head.append("apikey", apikey.value);
+	return await fetch(issuer + "module_question?module_id=eq." + (id ?? 0) + opstion, {
 		headers: head,
 	});
 }
